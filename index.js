851e717e1b741a5086e3f721af528e10354e8039
Tymoteusz Dzienniak <dzienniak.tymoteusz@gmail.com>
2020-12-02 11:07:45 +0100

    rewrite to es6+

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const enhancedAdapter = function (adapter) {
-  return function (config) {
-    const stack = new Error().stack;
-    const promise = adapter(config);
-    
-    return promise.catch(function (err) {
+const makeEnhancedAdapter = (adapter) => async (config) => {
+  const { stack } = new Error();
+  
+  try {
+    return await adapter(config);
+  } catch (err) {
+    if (err.isAxiosError) {
       /**
        * Generated stack trace will look something like that:
        * 
        *   Error:
-       *     at enhancedAdapter (/home/ubuntu/better-axios-error/index.js:54:21)
+       *     at makeEnhancedAdapter (/home/ubuntu/better-axios-error/index.js:54:21)
        *     at dispatchRequest (/home/ubuntu/better-axios-error/node_modules/axios/lib/core/dispatchRequest.js:52:10)
        *     at async c (/home/ubuntu/better-axios-error/index.js:66:3)
        *     at async b (/home/ubuntu/better-axios-error/index.js:70:3)
@@ -16,17 +17,18 @@ const enhancedAdapter = function (adapter) {
        * 
        * so we want do drop first 3 lines.
        */
-      const cleanStack = 'Thrown at:\n' + stack.split('\n').slice(3).join('\n');
-      err.stack = err.stack + '\n' + cleanStack;
-      throw err;
-    });
+      const cleanStack = `Thrown at:\n${stack.split('\n').slice(3).join('\n')}`;
+      err.stack = `${err.stack}\n${cleanStack}`;
+    }
+
+    throw err;
   }
 }
 
-function enhanceAxios(axiosInstance) {
+const enhanceAxios = (axiosInstance) => {
   const currentAdapter = axiosInstance.defaults.adapter;
 
-  axiosInstance.defaults.adapter = enhancedAdapter(currentAdapter);
+  axiosInstance.defaults.adapter = makeEnhancedAdapter(currentAdapter);
 
   return axiosInstance;
 }
